b126495c3f6da689d22c4635cb13dbebaffce0af
Tr1stan21 <javichd21@gmail.com>
2024-12-04 10:08:23 +0100

    commit inicial

---
+++ calculadora/src/main/java/com/example/Main.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in());
+
+        System.out.println("Ingresa el primer operando: ");
+        int numero1 = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Ingresa el operador: ");
+        char operador = scanner.CharAt(0);
+
+        System.out.println("Ingresa el segundo operando: ");
+        int numero2 = Integer.parseInt(scanner.nextLine());
+
+        int resultado;
+
+        if (operador = '+') {
+            resultado = numero1 + numero2;
+        } else if (operador = '-') {
+            resultado = numero1 + numero2;
+        }
+
+        System.out.println("El resutado es: " + resultado);
+    }
+}
